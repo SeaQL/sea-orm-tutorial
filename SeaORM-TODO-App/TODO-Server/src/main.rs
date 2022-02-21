59f39386868b661aa676a2ddcdceb51269e7eb0f
Charles Chege <charleschege@protonmail.ch>
2022-02-21 12:13:45 +0300

    Feat: Handle the server
    Feat: Handle database insertions
    
    Feat: Handle Client connections
    
    Feat: Enable chrono support

--- SeaORM-TODO-App/TODO-Server/src/main.rs
+++ SeaORM-TODO-App/TODO-Server/src/main.rs
@@ -1,13 +1,16 @@
 mod fruits_table;
+mod insert_values;
 mod server;
 mod suppliers_table;
 mod todos_table;
 
 pub use fruits_table::prelude::*;
+pub use insert_values::*;
 pub use server::*;
 pub use suppliers_table::prelude::*;
 pub use todos_table::prelude::*;
 
+use async_std::sync::Arc;
 use sea_orm::{
     sea_query::{Alias, ColumnDef, ForeignKey, ForeignKeyAction, Table},
     ConnectionTrait, Database, DbBackend,
@@ -27,117 +30,12 @@ async fn main() -> anyhow::Result<()> {
 
     // Perform a database connection
     let db = Database::connect(database_url).await?;
+    let db = Arc::new(db);
 
-    // Create the fruits table
-    let fruits_table = Table::create()
-        .table(Alias::new("fruits"))
-        .if_not_exists()
-        .col(
-            ColumnDef::new(Alias::new("fruit_id"))
-                .integer()
-                .auto_increment()
-                .primary_key()
-                .not_null(),
-        )
-        .col(
-            ColumnDef::new(Alias::new("fruit_name"))
-                .string()
-                .unique_key()
-                .not_null(),
-        )
-        .col(
-            ColumnDef::new(Alias::new("date_time"))
-                .timestamp()
-                .not_null(),
-        )
-        .to_owned();
+    insert_fruits(&db).await?;
+    insert_suppliers(&db).await?;
 
-    // Executing the SQL query to create the `fruits_table` in PostgreSQL
-    let create_table_op = db.execute(db_postgres.build(&fruits_table)).await;
-    // Print the result in a user friendly way
-    println!(
-        "`CREATE TABLE fruits` {:?}",
-        match create_table_op {
-            Ok(_) => "Operation Successful".to_owned(),
-            Err(e) => format!("Unsuccessful - Error {:?}", e),
-        }
-    );
-
-    // Create the `suppliers` table
-    let suppliers_table = Table::create()
-        .table(Alias::new("suppliers"))
-        .if_not_exists()
-        .col(
-            ColumnDef::new(Alias::new("suppliers_id"))
-                .integer()
-                .auto_increment()
-                .primary_key()
-                .not_null(),
-        )
-        .col(
-            ColumnDef::new(Alias::new("suppliers_name"))
-                .string()
-                .unique_key()
-                .not_null(),
-        )
-        .col(ColumnDef::new(Alias::new("fruit_id")).integer().not_null())
-        .col(
-            ColumnDef::new(Alias::new("date_time"))
-                .timestamp()
-                .not_null(),
-        )
-        .foreign_key(
-            ForeignKey::create()
-                .name("FK_supplier_fruits")
-                .from(Alias::new("suppliers"), Alias::new("fruit_id"))
-                .to(Alias::new("fruits"), Alias::new("fruit_id"))
-                .on_delete(ForeignKeyAction::Cascade)
-                .on_update(ForeignKeyAction::Cascade),
-        )
-        .to_owned();
-
-    // Executing the SQL query to create the `suppliers` table in PostgreSQL
-    let create_table_op = db.execute(db_postgres.build(&suppliers_table)).await;
-    // Print the result in a user friendly way
-    println!(
-        "`CREATE TABLE suppliers` {:?}",
-        match create_table_op {
-            Ok(_) => "Operation Successful".to_owned(),
-            Err(e) => format!("Unsuccessful - Error {:?}", e),
-        }
-    );
-
-    // Create the `todos` table
-    let todos_table = Table::create()
-        .table(Alias::new("todos"))
-        .if_not_exists()
-        .col(
-            ColumnDef::new(Alias::new("todo_id"))
-                .integer()
-                .auto_increment()
-                .primary_key()
-                .not_null(),
-        )
-        .col(
-            ColumnDef::new(Alias::new("username"))
-                .string()
-                .unique_key()
-                .not_null(),
-        )
-        .col(ColumnDef::new(Alias::new("todo_list")).string())
-        .to_owned();
-
-    // Executing the SQL query to create the `todos` table in PostgreSQL
-    let create_table_op = db.execute(db_postgres.build(&todos_table)).await;
-    // Print the result in a user friendly way
-    println!(
-        "`CREATE TABLE todos` {:?}",
-        match create_table_op {
-            Ok(_) => "Operation Successful".to_owned(),
-            Err(e) => format!("Unsuccessful - Error {:?}", e),
-        }
-    );
-
+    start_server(db).await?;
 
     Ok(())
 }
