8f8089396f8aabd99706afeed2a77a31a13eb2cb
Sanford Pun <punsanford@gmail.com>
2022-05-31 19:37:55 +0800

    Add bakery-backend source code

---
+++ bakery-backend/src/main.rs
@@ -0,0 +1,46 @@
+use futures::executor::block_on;
+use sea_orm::{ConnectionTrait, Database, DbBackend, DbErr, Statement};
+
+const DATABASE_URL: &str = "mysql://root:root@localhost:3306";
+
+async fn run() -> Result<(), DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
+
+    let db_name = "bakeries_db";
+    let db = &match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", db_name),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, db_name);
+            Database::connect(&url).await?
+        }
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", db_name),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", db_name),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, db_name);
+            Database::connect(&url).await?
+        }
+        DbBackend::Sqlite => db,
+    };
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = block_on(run()) {
+        panic!("{}", err);
+    }
+}
