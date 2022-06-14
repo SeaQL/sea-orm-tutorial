c8227cbd1949115d5815d4cba4d57201b5e3ff36
Sanford Pun <punsanford@gmail.com>
2022-06-14 14:24:52 +0800

    Remove legacy example source files

--- simple-crud/src/fruits_table/prelude.rs
+++
@@ -1,4 +0,0 @@
-pub use super::fruits::{
-    ActiveModel as FruitsActiveModel, Column as FruitsColumn, Entity as Fruits,
-    Model as FruitsModel, PrimaryKey as FruitsPrimaryKey, Relation as FruitsRelation,
-};
